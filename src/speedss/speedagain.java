083537c0a89bdcb3006a97443ece5478229ce3db
manvika123 <manvikajaiswal1995@gmail.com>
2017-09-17 13:15:27 +0530

    first commit

---
+++ src/speedss/speedagain.java
@@ -0,0 +1,13 @@
+package speedss;
+
+public class speedagain {
+
+    public static void main(String[] args) {
+
+        double dist= 24.0*1.6;
+        double time= 1.0+(40.0/60.0);
+        double avgspeed= dist/time;
+
+        System.out.println("avg speed is "+ avgspeed);
+    }
+}
